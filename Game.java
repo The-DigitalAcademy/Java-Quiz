ee14ee6e5eef6bd8f1b0ad75721b74c0b7f7dadd
Matukane00 <jabulile.matukane@gmail.com>
2024-06-12 15:59:05 +0200

    implementgame

--- Game.java
+++ Game.java
@@ -4,6 +4,15 @@ public class Game {
      // Main method to execute the program
     public static void main(String[] args) {
 
+      //Create an instance of QuestionService
+       QuestionsService service = new QuestionsService();
+       
+       //Call the playQuiz method to start the quiz
+       service.playQuiz();
+
+       //Call the printScore method to display the user'd score
+       service.printScore();
+
     }
 
 }
